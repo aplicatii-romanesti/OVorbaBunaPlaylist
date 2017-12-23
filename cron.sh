58710c28db38268017d0569e378c9bf9d913e546
www.bibliotecaortodoxa.ro <aplicatii.romanesti@gmail.com>
2017-12-23 20:21:10 +0200

    cron wrapper

--- cron.sh
+++ cron.sh
@@ -1,30 +1,3 @@
-#http://www.romania-actualitati.ro/Podcast/o_vorba_buna/14
-#1  - Agenda Globală
-#2  - Jurnalul de Prânz (not in use)
-#3  - Jurnalul de Seară
-#4  - Istorica
-#5  - Documentarele Radio România Actualităţi
-#6  - not in use
-#7  - not in use
-#8  - Euroatlantica
-#9  - Săptămâna Mediteranei (not in use)
-#10 - Lumea pe verticală(not in use)
-#11 - Radiojurnal (not in use)
-#12 - 150 Cuvinte
-#13 - Lecția de gramatică (not in use)
-#14 - O vorba buna
-#15 - Bună ziua căciulă că stăpănul n-are gură.Cod de bune maniere
-#16 - Alo? Rubrică satirică scrisă si interpretată de Maria Radu (not in use)
-#17 - Poveşti cu care creşti
-#18 - La dispoziţia dumneavoastră: Reţeta de sănătate
-#20 - Un minut de sănătate
-#21 - Semnele lumii
-#22 - Probleme la zi
-#23 - Europa, încotro ?
-#24 - Accente europene
-#25 - Serviciul de noapte
-#SERII="12 14" # important e 14!
-
 set -e
 cd /tmp/
 eval $(ssh-agent -s)
@@ -38,13 +11,10 @@ git config --global github.email aplicatii.romanesti@gmail.com
 git clone https://github.com/aplicatii-romanesti/OVorbaBunaPlaylist.git
 cd OVorbaBunaPlaylist
 
-for SERIE in {1..25}; do
-#for SERIE in 12 14; do
-  curl -sL http://www.romania-actualitati.ro/Podcast/o_vorba_buna/$SERIE | grep mp3 >>cron_${SERIE}_ovb.html
-  sort cron_${SERIE}_ovb.html | uniq > cron_${SERIE}_ovb.html_uniq
-  mv cron_${SERIE}_ovb.html_uniq cron_${SERIE}_ovb.html
-  git add cron_${SERIE}_ovb.html
-done
+chmod +x ./cron_tash.sh
+./cron_tash.sh
+git add .
+
 git commit -m"`date`" --author "aplicatii-romanesti"
 git push
 mv ~/.gitconfig.cron ~/.gitconfig
