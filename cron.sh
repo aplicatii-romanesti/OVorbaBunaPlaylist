a10d6ec0c4800f62a96b342168677f4ec7de4ac3
aplicatii-romanesti <aplicatii.romanesti@gmail.com>
2017-12-30 20:39:41 +0200

    git via ssh

--- cron.sh
+++ cron.sh
@@ -9,7 +9,7 @@ git config --global user.email aplicatii.romanesti@gmail.com
 git config --global github.user aplicatii-romanesti
 git config --global github.email aplicatii.romanesti@gmail.com
 
-git clone https://github.com/aplicatii-romanesti/OVorbaBunaPlaylist.git
+git clone git@github.com:aplicatii-romanesti/OVorbaBunaPlaylist.git
 cd OVorbaBunaPlaylist
 
 chmod +x ./cron_tash.sh
