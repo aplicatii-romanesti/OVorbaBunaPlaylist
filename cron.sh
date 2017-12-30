4e0ca1b5ea52ea3b8e71a9e9aa984080ab9e3ccd
www.bibliotecaortodoxa.ro <aplicatii.romanesti@gmail.com>
2017-12-30 20:50:24 +0200

    typo

--- cron.sh
+++ cron.sh
@@ -13,7 +13,7 @@ git clone git@github.com:aplicatii-romanesti/OVorbaBunaPlaylist.git
 cd OVorbaBunaPlaylist
 
 chmod +x ./cron_task.sh
-./cron_tash.sh
+./cron_task.sh
 git add .
 
 git commit -m"`date`" --author "aplicatii-romanesti"
