2bdd6b1c0d45dc6536f8ed485141a1fa01320c96
aplicatii-romanesti <aplicatii-romanesti@gmail.com>
2018-09-22 01:00:04 +0300

    dbg

--- cron.sh
+++ cron.sh
@@ -8,10 +8,10 @@ rm -rf ${PRJ}
 #eval $(ssh-agent -s)
 #ssh-add ~/.ssh/aplicatii-romanesti
 #mv ~/.gitconfig ~/.gitconfig.cron
-#git config --global user.name aplicatii-romanesti
-#git config --global user.email aplicatii.romanesti@gmail.com
-#git config --global github.user aplicatii-romanesti
-#git config --global github.email aplicatii.romanesti@gmail.com
+\git config --global user.name aplicatii-romanesti
+\git config --global user.email aplicatii.romanesti@gmail.com
+\git config --global github.user aplicatii-romanesti
+\git config --global github.email aplicatii.romanesti@gmail.com
 
 \git clone git@github.com:aplicatii-romanesti/${PRJ}.git
 cd ${PRJ}
