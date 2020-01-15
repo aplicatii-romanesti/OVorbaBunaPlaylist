109860735398786be74f10b3b738250b1a378ab2
aplicatii-romanesti <aplicatii.romanesti@gmail.com>
2020-01-15 23:02:47 +0200

    commit cron

--- cron.sh
+++ cron.sh
@@ -8,19 +8,24 @@ rm -rf ${PRJ}
 #eval $(ssh-agent -s)
 #ssh-add ~/.ssh/aplicatii-romanesti
 #mv ~/.gitconfig ~/.gitconfig.cron
-#git config --global user.name aplicatii-romanesti
-#git config --global user.email aplicatii.romanesti@gmail.com
-#git config --global github.user aplicatii-romanesti
-#git config --global github.email aplicatii.romanesti@gmail.com
+\git config --global user.name aplicatii-romanesti
+\git config --global user.email aplicatii.romanesti@gmail.com
+\git config --global github.user aplicatii-romanesti
+\git config --global github.email aplicatii.romanesti@gmail.com
 
 \git clone git@github.com:aplicatii-romanesti/${PRJ}.git
 cd ${PRJ}
 
+echo "start: $(date) " >> last_run_date.log
+
 chmod +x ./cron_task.sh
-echo "demo ./cron_task.sh"
+./cron_task.sh
+
+echo "finish: $(date) " >> last_run_date.log
 \git add .
 
-\git commit -m"`date`" --author "aplicatii-romanesti"
+\git commit -m"`date`"
+# --author "aplicatii-romanesti"
 \git push
 \mv ~/.gitconfig.cron ~/.gitconfig
 
