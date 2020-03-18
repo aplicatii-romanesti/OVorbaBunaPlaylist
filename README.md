ee777d3c044547a933c20c818572ec99c18e0a0a
aplicatii-romanesti <aplicatii.romanesti@gmail.com>
2020-03-18 08:42:44 +0200

    README - updates

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# O Vorba Buna Playlist
+# Arhiva de Serii Extraordinare de emisiuni ale postului Radio Romania Actualitati
+
+Din pacate pe site-ul Radio Romania Actualitati nu sunt decat emisiunile ultimei saptamani. Astfel s-a pornit acest efort de a recupera si pune in valoare toata seria!
+
+## O Vorba Buna - Playlist
 O Vorba Buna Remus Radulescu Radio Romania Actualitati Playlist Arhiva
 
 Playlist-ul arhivei O Vorba Buna cu Remus Radulescu - emisiune la Radio Romania Actualitati - Podcasturile in format mp3
@@ -9,13 +13,27 @@ Contine:
 1. tot ce s-a putut gasi pana pe 17 Noiembrie 2017.
   (Sunt doar ~120, dar restul nu se pot deosebi din miile de mp3-uri in http://media.3netmedia.ro/media/RRAOnline/audio/)
 
-2. Sunt corecte: 6 dec 2017 -> 22 Feb 2018  si 10 Sept 2018 -> la zi.   
-  (deci lipsesc: 22Feb-10Sept 2018 )   
+2. Emisiuni: 6 dec 2017 -> 22 Feb 2018  si 10 Sept 2018 -> la zi.   
+  (lipsesc: 22Feb-10Sept 2018 )   
 ```
 >Ediţia din 22 februarie 2018</a><a alt="Download" href="http://media.3netmedia.ro/media/RRAOnline/audio/128992.mp3"
 <LIPSA>
 ">Ediţia din 10 septembrie 2018</a><a alt="Download" href="http://media.3netmedia.ro/media/RRAOnline/audio/133205.mp3"
 ```
+3. Manual: din miile de mp3-uri in http://media.3netmedia.ro/media/RRAOnline/audio, s-au ales cele din seria: "O Vorba Buna". Acest proces nu e finalizat, vom adauga pe masura ce vom progresa.    
+Metodologia utilizata: 
+- s-au ales numele fisierelor din perioadele pe care nu le aveam
+- s-a extras durata fiecarui fisier mp3
+- s-au selectat cele cu durata intre 40s - 116s # Majoritatea emisiunilor sunt in aceasta marja
+- s-au ascultat manual fiecare din cele filtrate mai sus si s-au marcat cele din emisiunea O Vorba Buna sau 150 Cuvinte
 
-In total peste 500 emisiuni.
+1+2+3: In total peste 960 emisiuni din minunatata serie: O Vorba Buna
 Va uram ascultare placuta!
+
+## 150 Cuvinte - Playlist
+Tot aici, mai puteti gasi si o parte din emisiunile "150 Cuvinte"
+
+Playlist-ul: https://raw.githubusercontent.com/aplicatii-romanesti/OVorbaBunaPlaylist/master/O_150_Cuvinte_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
+
+## Pentru mai multe informatii, ne puteti contacta pe email!
+
--- make_update_m3u.sh
+++ make_update_m3u.sh
@@ -6,9 +6,9 @@ CAT=14
 grep 'http://media.3netmedia.ro/media/RRAOnline/audio/' archive_cron_html/cron_${CAT}_ovb.html | cut -d'"' -f8 > ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp
 uniq ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp > ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp.tmp
 tac ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp.tmp > ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
-echo "# OLDER from archive.org" >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
+#echo "# OLDER from archive.org" >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
 grep '^http://media.3netmedia.ro/media/RRAOnline/audio/' ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_recovered_archive_org.m3u >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
-echo "# OLDER manual recovered" >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
+#echo "# OLDER manual recovered" >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
 grep '^http://media.3netmedia.ro/media/RRAOnline/audio/' ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_recovered_manual.m3u >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
 # Add silence
 cat ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u | sort -n | uniq | awk ' {print;} { print "https://raw.githubusercontent.com/aplicatii-romanesti/OVorbaBunaPlaylist/master/silence7s.mp3"; }' > ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp
@@ -20,9 +20,9 @@ CAT=12
 grep 'http://media.3netmedia.ro/media/RRAOnline/audio/' archive_cron_html/cron_${CAT}_ovb.html | cut -d'"' -f8 > ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp
 uniq ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp > ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp.tmp
 tac ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp.tmp > ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
-echo "# OLDER from archive.org - TODO" >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
+#echo "# OLDER from archive.org - TODO" >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
 #TODO: grep '^http://media.3netmedia.ro/media/RRAOnline/audio/' ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_recovered_archive_org.m3u >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
-echo "# OLDER manual recovered" >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
+#echo "# OLDER manual recovered" >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
 grep '^http://media.3netmedia.ro/media/RRAOnline/audio/' ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_recovered_manual.m3u >> ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u
 # Add Silence
 cat ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u | sort -n | uniq | awk ' {print;} { print "https://raw.githubusercontent.com/aplicatii-romanesti/OVorbaBunaPlaylist/master/silence7s.mp3"; }' > ${SET}_Radio_Romania_Actualitati_Playlist_Arhiva_all.m3u.tmp
